b931abf60cb76c148fa3bab6f3d5a15638b31a99
LelipalyEric <lelipalyeric@gmail.com>
2026-05-24 07:52:12 +0700

    Nambah Header Navbar Nested Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import { Router, Route } from "@solidjs/router";
 
+// Import pages simpen sini
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Home from "./pages/Home"
+import Home from "./pages/Home" 
+
+// Import components simpen sini
+import Navbar from "./components/Navbar";
+
+// Bikin component yang hanya di render kalau masuk main pages ( bukan login dan register / udah masuk si aplikasinya )
+const mainPages = (props) => {
+  return(
+    <>
+      <Navbar />
+      <div>
+        {props.children}
+      </div>
+    </>
+  );
+};
+
 function App() {
   return (
     <Router>
       <Route path="/" component={Login} />
       <Route path="/register" component={Register} />
-      <Route path="/home" component={Home} />
+
+      <Route component={mainPages}>
+        <Route path="/home" component={Home} />
+        {/*Kalau mau nambah page simpen disini biar dia ke load navbar*/}
+      </Route>
+
     </Router>
   );
 }
