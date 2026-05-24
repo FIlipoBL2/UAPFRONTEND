00b8657b543c014b2eb0073d1e540039fd54397c
bryan's_laptop <bryan>
2026-05-24 14:20:45 +0700

    compute avg

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,31 @@
-import { games } from "../data/mockData";
+import { games,reviews } from "../data/mockData";
 import { useParams } from "@solidjs/router";
 
 const Game = () => {
     const params = useParams();
     const selectedGames = games.find(game => game.id === Number(params.id));
+    
+    const getScoreColor = (score) => {
+        if (score < 60) return "#ff4d4d"; // Red
+        if (score < 75) return "#ffcc00"; // Yellow
+        return "#00ce67ff"; // Green
+    };
 
+    const computeAvg = () =>{
+        let totalScore = 0;
+        let reviewCount = 0;
+        reviews.forEach((val)=>{
+            if(val.gameId === selectedGames.id){
+                totalScore += val.score;
+                reviewCount++;
+            }
+        })
+        return Math.round(totalScore / reviewCount)
+    }
+
+    const avg = computeAvg();
     return (
+        <>
         <div style={{ display: "grid", "grid-template-columns": "1fr 1fr", "grid-template-rows": "1fr 1fr", "grid-template-areas": '"banner gameInfo" "reviews addReview"', margin: "0", padding: "20px", width: "100%", "box-sizing": "border-box" }}>
             
             <div style={{ "grid-area": "banner", display: "flex", "justify-content": "center", "align-items": "center", width: "100%", height: "100%", "background-color": "gray" }}>
@@ -29,13 +49,24 @@ const Game = () => {
             
                 <div style={{ "grid-area": "score", display: "flex", "flex-direction": "column", "align-items": "center", "padding-left": "16px" }}>
                     <h2>Average Score</h2>
-                    <div style={{ "background-color": "yellow", "border-radius": "12px", height: "40px", width: "40px", display: "flex", "justify-content": "center", "align-items": "center", "font-size": "24px", "font-weight": "bold" }}>
-                        <p>67</p>
+                    <div style={{ "background-color": getScoreColor(avg), "border-radius": "12px", height: "40px", width: "40px", display: "flex", "justify-content": "center", "align-items": "center", "font-size": "24px", "font-weight": "bold" }}>
+                        <p>{avg}</p>
                     </div>
                 </div>
                 
             </div>
         </div>
+        <div class="addReviewContainer">
+        <h2>My Score</h2>
+        <div class="slider">
+            <input type="range" min="0" max="10" value="5" class="sliderInput" />
+            <div class="sliderValue">5</div>
+        </div>
+        <div class="reviewBtnContainer">
+            <button>Add my Review</button>
+        </div>
+        </div>
+        </>
     );
 }
 
