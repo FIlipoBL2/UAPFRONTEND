df96d04cff67d37b053d02f42757d1a672cd1dd6
IEatSteak <vin24farrel@gmail.com>
2026-05-26 10:13:34 +0700

    ui improvements

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -36,76 +36,93 @@ const Game = () => {
     //harus dijadikan arrow function untuk menjadi reactive
     const avg = () => computeAvg();
     return (
-        <>
-        <div class="mainContainer">
-            
-            <div class="gameImageContainer">
-                <img src={selectedGames?.image} alt="Game Image" />
-            </div>
-            
-            <div class="gameInfoContainer">
-                
-                <h1>
-                    {selectedGames?.title}
-                </h1>
-                
-                <p class="platform">
-                    {deviceNames}
-                </p>
-            
-                <div class="releaseDateContainer">
-                    <h2>Release Date</h2>
-                    <h2>{selectedGames?.releaseDate}</h2>
+        <div class="background">
+            <div class="mainContainer">
+                <div class="gameImageContainer">
+                    <img src={selectedGames?.image} alt="Game Image" />
                 </div>
-            
-                <div class="scoreContainer">
-                    <h2>Average Score</h2>
-                    <div class="scoreBox" style={{ "background-color": getScoreColor(avg())}}>
-                        <p>{avg}</p>
+
+                <div class="gameInfoContainer">
+
+                    <h1>
+                        {selectedGames?.title}
+                    </h1>
+
+                    <p class="platform">
+                        {deviceNames}
+                    </p>
+
+                    <div class="releaseDateContainer">
+                        <h2>Release Date</h2>
+                        <h2>{selectedGames?.releaseDate}</h2>
+                    </div>
+
+                    <div class="scoreContainer">
+                        <h2>Average Score</h2>
+                        <div class="scoreBox" style={{ "background-color": getScoreColor(avg())}}>
+                            <p>{avg}</p>
+                        </div>
                     </div>
                 </div>
-                
-            </div>
 
-        </div>
-        <div class="addReviewContainer">
-        <h2>My Score</h2>
-        <div class="slider">
-            <input type="range" min="0" max="100" value={slider()} class="sliderInput" onInput={(e) => setSlider(e.target.value)}></input>
-            <div class="sliderValue">{Number(slider())}</div>
-        </div>
-        <div class="reviewBtnContainer">
-            <button onClick={() => setIsModalOpen(prev => !prev)}>Add my Review</button>
-        </div>
-        <Show when={IsModalOpen()}>
-            <ReviewModal game={selectedGames} setScore={setSlider} score={slider()}/>
-        </Show>
-        <div class="reviewsContainer">
-            <div class="reviewHeader">
-                Search Reviews
-            </div>
-
-            <div class="review">
-                <div class="scoreBox">
-                    <p>9.0</p>
+                <div class="addReviewContainer">
+                    <h2>My Score</h2>
+                    <div class="slider">
+                        <input type="range" min="0" max="100" value={slider()} class="sliderInput" onInput={(e) => setSlider(e.target.value)}></input>
+                        <div class="sliderValue">{Number(slider())}</div>
+                    </div>
+                    <div class="reviewBtnContainer">
+                        <button onClick={() => setIsModalOpen(prev => !prev)}>Add my Review</button>
+                    </div>
+                    <Show when={IsModalOpen()}>
+                        <ReviewModal game={selectedGames} setScore={setSlider} score={slider()}/>
+                    </Show>
                 </div>
-                <h3>Reviewer Name</h3>
-                <p>Review text goes here. This is a sample review to demonstrate the layout of the reviews section.</p>
-                <button class="readMoreBtn">Read More</button>
-            </div>
 
-            <div class="pagination">
-                <button>prev</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>next</button>
+                <div class="reviewsContainer">
+                    <div class="reviewHeader">
+                        Search Reviews
+                    </div>
+
+                    <div class="review">
+                        <div class="scoreBox">
+                            <p>9.0</p>
+                        </div>
+                        <h3>Reviewer Name</h3>
+                        <p>Review text goes here. This is a sample review to demonstrate the layout of the reviews section.</p>
+                        {/* <button class="readMoreBtn">Read More</button> */}
+                    </div>
+
+                    <div class="review">
+                        <div class="scoreBox">
+                            <p>9.0</p>
+                        </div>
+                        <h3>Reviewer Name</h3>
+                        <p>Review text goes here. This is a sample review to demonstrate the layout of the reviews section.</p>
+                        {/* <button class="readMoreBtn">Read More</button> */}
+                    </div>
+
+                    <div class="review">
+                        <div class="scoreBox">
+                            <p>9.0</p>
+                        </div>
+                        <h3>Reviewer Name</h3>
+                        <p>Review text goes here. This is a sample review to demonstrate the layout of the reviews section.</p>
+                        {/* <button class="readMoreBtn">Read More</button> */}
+                    </div>
+
+                    <div class="pagination">
+                        <button>prev</button>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>next</button>
+                    </div>
+                </div>
             </div>
         </div>
-
-        </div>
-        </>
     );
 }
 
